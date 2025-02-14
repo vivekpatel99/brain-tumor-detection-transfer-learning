e603e1839babb496560776ff023e4e339b79ac81
vivekpatel99 <vivpatel99@gmail.com>
2025-02-14 08:23:58 +0000

    okesh working coordinates

--- utils/custom_dataset_loader.py
+++ utils/custom_dataset_loader.py
@@ -66,10 +66,10 @@ class PrepareDataset:
                 - A list of class ids.
                 - A list of bounding boxes, where each bounding box is an array of 8 floats.
         """
-        image_paths = []
+        images = []
         class_ids = []
         bboxes = []
-        for file_name in tqdm(self.image_dir.iterdir()):
+        for file_name in tqdm(list(self.image_dir.iterdir())[:100]):
             if file_name.suffix.endswith((".jpg", ".png")):
                 image =  cv2.resize(cv2.imread(str(file_name)), self.dst_img_size)
                 # preprocessed_img = self.prepare_images(image_path=imaga_file_path)
@@ -85,10 +85,6 @@ class PrepareDataset:
                 if not lines:
                     continue
 
-                # NOTE: for the time being avoid using ragged batches, that's why these line commented out
-                # image_bboxes = []
-                # image_classes = []
-
                 # 1 0.4395833333333333 0.35625 0.12916666666666668 0.1125
                 for line in lines:
                     try:
@@ -96,42 +92,12 @@ class PrepareDataset:
                         class_id = values[0]
                         coords = values[1:5]  # Coords are already normalized YOLO format
 
-                        if coords.size == 8:
-                            coords = coords.reshape(4, 2)  # Ensure it's a 2D array with four points
-
-                        # Calculate xmin, ymin, xmax, ymax
-                        x_coords = coords[:, 0]*self.dst_img_size[0]
-                        y_coords = coords[:, 1]*self.dst_img_size[1]
-                        xmin = np.min(x_coords)
-                        ymin = np.min(y_coords)
-                        xmax = np.max(x_coords)
-                        ymax = np.max(y_coords)
-
-                        # Calculate YOLO format coordinates
-                        x_center = (xmin + xmax) / 2.0
-                        y_center = (ymin + ymax) / 2.0
-                        width = xmax - xmin
-                        height = ymax - ymin
-
-                        # Normalize coordinates
-                        x_center /= self.dst_img_size[0]
-                        y_center /= self.dst_img_size[1]
-                        width /= self.dst_img_size[0]
-                        height /= self.dst_img_size[1]
-                        # bboxes.append(coords)
-                        # image_classes.append(class_id)
-                        # image_paths.append(str(imaga_file_path))
-                        bboxes.append([x_center, y_center, width, height])
-                        # bboxes.append([xmin, ymin, xmax, ymax])
+                        bboxes.append(coords)
                         class_ids.append(class_id)
-                        image_paths.append(image)
+                        images.append(image)
 
                     except Exception as e:
                         print(f"[ERROR] - {e} in file {label_file_path} on line: {line}")
                         continue
                     
-                # image_paths.append(imaga_file_path)
-                # class_ids.append(image_classes)
-                # bboxes.append(np.concatenate(image_bboxes, axis=0))  # Concatenate boxes for the image
-
-        return np.array(image_paths), np.array(class_ids,dtype=np.int8), np.array(bboxes, dtype=np.float32)
+        return np.array(images), (np.array(class_ids,dtype=np.int8), np.array(bboxes, dtype=np.float32))
